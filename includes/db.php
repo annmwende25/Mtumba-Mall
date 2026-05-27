36a222a0b171888a8f729c1fe664935f3a181ad2
annmwendebariu@gmail.com <annmwendembariu@gmail.com>
2026-05-27 16:55:27 +0300

    :wq

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,12 @@
 <?php
 
 $host = "localhost";
+$port = 3306;
 $user = "root";
 $password = "";
 $database = "mtumba_mall";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
